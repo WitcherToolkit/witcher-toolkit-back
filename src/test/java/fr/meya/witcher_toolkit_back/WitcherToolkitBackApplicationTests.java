dd39987ded4a9c776c52b15dbc246fdff729bc84
mdelage <angie-island@hotmail.fr>
2024-11-20 13:40:42 +0100

    first commit

---
+++ src/test/java/fr/meya/witcher_toolkit_back/WitcherToolkitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.meya.witcher_toolkit_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitcherToolkitBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
