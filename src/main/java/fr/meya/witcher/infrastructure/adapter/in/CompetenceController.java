998ddf9208bed55c8a55f47d42e4489c5092877f
Mélody Delage <angie-island@hotmail.fr>
2025-06-30 15:15:46 +0200

    lecture liste et détail comptétences

--- src/main/java/fr/meya/witcher/infrastructure/adapter/in/CompetenceController.java
+++ src/main/java/fr/meya/witcher/infrastructure/adapter/in/CompetenceController.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/competence")
+@RequestMapping("/competences")
 public class CompetenceController {
 
 	private final ICompetenceService iCompetenceService;
